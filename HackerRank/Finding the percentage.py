198f14da632086df14bf639065708106cf5bd465
quanta <quantagonista@gmail.com>
2018-06-14 13:52:51 +0600

    null

---
+++ HackerRank/Finding the percentage.py
@@ -0,0 +1,10 @@
+n = int(input())
+student_marks = {}
+for _ in range(n):
+    name, *line = input().split()
+    scores = list(map(float, line))
+    student_marks[name] = scores
+query_name = input()
+marks = student_marks[query_name]
+average = sum(marks) / len(marks)
+print('{0:.2f}'.format(average))
